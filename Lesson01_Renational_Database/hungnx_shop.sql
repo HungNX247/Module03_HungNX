45c5158a002b83cdd728e5f351d759602d82f371
HungNX247 <hungnx6666@gmail.com>
2025-12-04 17:25:58 +0700

    add

--- Lesson01_Renational_Database/hungnx_shop.sql
+++ Lesson01_Renational_Database/hungnx_shop.sql
@@ -18,3 +18,36 @@ select * from products;
 
 update products 
 set price = 30000000 where id = 3;
+
+create table customers (
+id int,
+name varchar(100),
+birthday datetime,
+address text,
+phone int);
+
+-- drop table customers;
+
+insert into customers (id,name, birthday, address, phone) values 
+(1, 'Nguyễn Xuân Hùng', '1991-06-28','Hải Phòng', '0766088439'),
+(2, 'Tạ Thu Mơ','1998-10-07','Hà Nội','0334992975');
+
+select * from users;
+
+delete from customers where id = 1;
+
+update customers set birthday = '1998-10-08' where id = 2;
+
+alter table customers add email text;
+alter table customers add gender varchar(100),
+add details text;
+
+alter table customers drop column details;
+
+alter table customers change column name fullname varchar(100);
+
+alter table customers modify phone varchar(20);
+
+alter table customers rename to users;
+
+alter table users add primary key (id);
